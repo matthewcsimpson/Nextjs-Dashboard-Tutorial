74176844e1535d8ecba3b4f83b4a36e0bc896508
Matthew Simpson <mcsimpson+gh@gmail.com>
2024-06-26 21:24:33 -0700

    chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+// Styles & Fonts
 import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+
 
 
 export default function RootLayout({
@@ -8,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
