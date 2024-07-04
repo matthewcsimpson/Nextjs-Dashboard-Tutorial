ca75f2cec7f55c8649072f0c6e25a22a05f41df1
Matthew Simpson <mcsimpson+gh@gmail.com>
2024-07-04 04:02:14 -0700

    hotfix enoent errpr

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,8 +4,8 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 // Experimental
 export const experimental_ppr = true;
 
- 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
